6c13022b827f7619ad4419d9950e90d875346073
Anderson Wang <atw293@nyu.edu>
2018-12-31 19:00:11 -0500

    Save sprite images

--- VeekunToExcel.py
+++ VeekunToExcel.py
@@ -3,6 +3,7 @@
 from pprint import pprint
 import time
 import os
+import urllib.request
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
@@ -21,33 +22,36 @@ else:
 
 url = "https://veekun.com/dex/pokemon/search?sort=evolution-chain&introduced_in=1&introduced_in=2&introduced_in=3&introduced_in=4&introduced_in=5&introduced_in=6&introduced_in=7"
 driver.get(url)
-time.sleep(2)
+driver.implicitly_wait(10)
 
 xpath = '//*[@id="content"]/table/tbody/tr'
-list = driver.find_elements_by_xpath('//*[contains(@class,"evolution")]')
+entries = driver.find_elements_by_xpath('//*[contains(@class,"evolution")]')
 # pprint(list[0].get_attribute('innerHTML'))
 #Program must click into each individual pokemon to retrieve Number and Artwork
-for l in list:
-    pprint(l.get_attribute('innerHTML'))
-    name = l.find_element_by_css_selector('td.name')
-    print(name.text)
-    link = name.find_element_by_css_selector('a')
-    action = ActionChains(driver)
-    action\
+for entry in entries:
+    link = entry.find_element_by_css_selector('td.name a')
+    #NAME
+    name = link.text
+    action1 = ActionChains(driver)
+    action1\
          .move_to_element(link)\
          .key_down(Keys.COMMAND)\
          .click(link)\
          .key_up(Keys.COMMAND)\
          .perform()
-    time.sleep(3)
     driver.switch_to.window(driver.window_handles[1])
-    time.sleep(4)
+
+    #DEX_NUM
     dex_num = driver.find_element_by_xpath('//*[@id="content"]/div[4]/div[1]/dl[1]/dd[2]').text
-    pprint(type(dex_num))
-    time.sleep(2)
+
+    image = driver.find_element_by_xpath('//*[@id="dex-pokemon-portrait-sprite"]/img')
+    src = image.get_attribute('src')
+    urllib.request.urlretrieve(src, "{}/{}.png".format(dirName,name))
+
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
-    time.sleep(3)
+    pprint(name)
+    pprint(dex_num)
     # driver.execute_script("window.history.go(-1)")
 
 # driver.quit()
